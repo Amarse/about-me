0d20c0ecea99fa4769bd50adf1c31a624b0d45c2
Amarse <byicy08@gmail.com>
2022-12-19 20:15:51 +0900

    firebase connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import { useEffect } from "react";
+import { fireStore } from "./FireBase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    console.log(fireStore);
+  });
+
+  return <div className="App">{fireStore._databaseId.projectId}</div>;
 }
 
 export default App;
